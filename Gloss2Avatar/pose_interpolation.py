81868857861a8068fdf024135784451b62acbd93
yellow <yellow@yellow.cs.columbia.edu>
2020-07-27 20:57:26 -0400

    bugfix

--- Gloss2Avatar/pose_interpolation.py
+++ Gloss2Avatar/pose_interpolation.py
@@ -7,23 +7,23 @@ from torch.utils.data import Dataset, SubsetRandomSampler, DataLoader
 # Input: (B, 2, 137, 3)
 # Output: (B, 10, 137, 3) 
 class PoseInterpolation(nn.Module):
-  def __init__(self, num_hidden_layers=8, hidden_dim=512):
+  def __init__(self, num_hidden_layers=5, hidden_dim=512):
     super(PoseInterpolation, self).__init__()
     self.num_hidden_layers = num_hidden_layers
     self.input_layer = nn.Linear(2*137*3, hidden_dim)
-    self.hidden_layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim)])
+    self.hidden_layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim) for i in range(num_hidden_layers)])
     self.output_layer = nn.Linear(hidden_dim, 10*137*3)
     self.act = torch.selu
-    self.alpha_dropout = nn.AlphaDropout(0.1)
+    self.alpha_dropout = nn.AlphaDropout(0.0)
 
   def forward(self, x):
     batch_size = x.size(0)
     x = x.view(batch_size, 2*137*3)
     x = self.input_layer(x)
-    x = self.alpha_dropout(self.act(x))
+    x = self.act(self.alpha_dropout(x))
     for layer in self.hidden_layers:
       x = layer(x)
-      x = self.alpha_dropout(self.act(x))
+      x = self.act(self.alpha_dropout(x))
     x = self.output_layer(x)
     x = x.view(batch_size, 10, 137, 3)
     return x
@@ -61,7 +61,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.mse_loss(output, target, reduction='mean').item()  # sum up batch loss
+            test_loss += F.mse_loss(output, target, reduction='sum').item()  # sum up batch loss
 
     test_loss /= len(test_loader.dataset)
 
@@ -74,18 +74,18 @@ if __name__ == "__main__":
   parser.add_argument('--train_test_split', type=float, default=0.9)
   parser.add_argument('--epochs', type=int, default=100)
   parser.add_argument('--gpu', type=int, default=0)
-  parser.add_argument('--num_hidden_layers', type=int, default=8)
+  parser.add_argument('--num_hidden_layers', type=int, default=5)
   parser.add_argument('--hidden_dim', type=int, default=512)
   parser.add_argument('--batch_size', type=int, default=64)
   parser.add_argument('--num_workers', type=int, default=4)
-  parser.add_argument('--log_interval', type=int, default=100)
+  parser.add_argument('--log_interval', type=int, default=50)
   parser.add_argument('--seed', type=int, default=1)
   args = parser.parse_args()
 
   torch.manual_seed(args.seed)
 
   model = PoseInterpolation(args.num_hidden_layers, args.hidden_dim).to(args.gpu)
-  optimizer = torch.optim.Adam(model.parameters())
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
   data = InterpolationDataset(args.dataset_path)
   train_test_split = int(len(data) * args.train_test_split)
   indices = list(range(len(data)))
