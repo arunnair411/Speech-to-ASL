b52a0c22be08c4ba2115b5e633080c0ba6ec1ade
Arun <t-arnair@microsoft.com>
2020-07-29 12:53:53 -0700

    Updated gloss_lookup.py with more glosses and a bugfix

--- Gloss2Avatar/data/RawData/gloss_lookup.py
+++ Gloss2Avatar/data/RawData/gloss_lookup.py
@@ -1,4 +1,5 @@
 import torch
+import pdb
 metadata = torch.load('Liz.data')
 gloss_list = metadata['gloss']
 filename_list = metadata['filename']
@@ -8,7 +9,32 @@ def find_str(l, pat):
    return i
  return -1
 
-gloss_sequence = ['we','receive','documents']
+# gloss_sequence = ['we','receive','documents']
+gloss_sequence = ['point-to','get', 'paper']
 for g in gloss_sequence:
-  print(filename_list[find_str(gloss_list, g)])
+    # print(filename_list[find_str(gloss_list, g)])
+    print(filename_list[gloss_list.index(g.upper())])
 
+
+gloss_sequence = ['car','point-to','drives']
+for g in gloss_sequence:
+    # print(filename_list[find_str(gloss_list, g)])
+    print(filename_list[gloss_list.index(g.upper())])
+
+# gloss_sequence = ['we','receive','documents']
+gloss_sequence = ['point-to','get', 'paper']
+for g in gloss_sequence:
+    # print(filename_list[find_str(gloss_list, g)])
+    print(filename_list[gloss_list.index(g.upper())])
+
+# How are you?
+gloss_sequence = ['how','point-to']
+for g in gloss_sequence:
+    # print(filename_list[find_str(gloss_list, g)])
+    print(filename_list[gloss_list.index(g.upper())])
+
+# "I forwarded the texts to you."  >>15439,7322,2917
+gloss_sequence = ['texting','go-to-it', 'X']
+for g in gloss_sequence:
+    # print(filename_list[find_str(gloss_list, g)])
+    print(filename_list[gloss_list.index(g.upper())])
