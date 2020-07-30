712209117522891966f638ee065cb7d74dfcd0a5
arunnair411 <arunnair411@gmail.com>
2020-07-30 19:35:25 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ python English2Gloss/translate.py -data_pkl English2Gloss/eng2gloss_data.pkl -mo
 # align glosses to poses
 
 # gloss to individual poses
-
+python Gloss2Avatar/gloss_lookup.py --glosses='car,point-to,drive'
 # pose interpolation
 
 # data prep for pose2avatar pix2pix model
-
+python Gloss2Avatar/create_test_data.py --output-dir=/path/to/dir/containing/pose/images/ --file-ids='207,15602,15316' --pose-data=car_she_drives.posesequence
 # pose2avatar frames
 python Pose2Avatar/test.py --dataroot /path/to/dir/containing/pose/images/ --name pose2avatar --model test --netG unet_256 --direction AtoB --dataset_mode single --norm batch
 # avatar frames to avatar video
+python Gloss2Avatar/generate_video.py --results-dir /path/to/dir/containing/network/outputs
